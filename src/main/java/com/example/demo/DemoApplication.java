a02ae8e01d46d1a40391f8802dd3ca62a7588a08
chanlzl <liuzhilong1229@163.com>
2020-12-05 15:38:09 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    @GetMapping("/docker/hello")
+    public String helloDocker() {
+        return "hello docker";
+    }
+
+}
